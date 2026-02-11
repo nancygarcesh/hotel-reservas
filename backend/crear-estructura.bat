1e68c70dcc89dc251b6432068570e1b734ad5f73
Nancy <nghazou35@gmail.com>
2026-02-11 17:00:08 -0400

    backend files created

---
+++ backend/crear-estructura.bat
@@ -0,0 +1,17 @@
+@echo off
+mkdir src
+cd src
+
+mkdir config
+mkdir controllers
+mkdir routes
+mkdir models
+mkdir middlewares
+mkdir services
+mkdir utils
+mkdir database
+mkdir validators
+mkdir uploads
+
+echo Estructura de carpetas creada exitosamente!
+pause
