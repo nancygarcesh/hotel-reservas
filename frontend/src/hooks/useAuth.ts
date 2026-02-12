c42b3c6c5b5a7eb2647e73c410f6ac95b9cbccbb
Nancy <nghazou35@gmail.com>
2026-02-12 00:21:46 -0400

    deleted files

--- frontend/src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthStore } from "@/store/useAuthStore";
-
-export const useAuth = () => useAuthStore();
-export const getToken = () => localStorage.getItem("token");
-export const logout = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  window.location.href = "/login";
-};
